a0c19fcf9f1888797b44a9e45a88e9f3b7c4d30a
enrique96 <3enrique96@gmail.com>
2016-11-03 17:58:53 -0600

    IR_Sensors
    Code to get readings from the IR_Sensors

---
+++ IR_sensors/IR_sensors.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  //float result;
+  Serial.begin(9600);
+
+}
+           
+void loop() {
+  // put your main code here, to run repeatedly:
+float result=0;
+  for(int i; i<1000; i++)
+  {
+    result=result+analogRead(0);
+  }
+  result=result/1000;
+  Serial.print(result);
+  Serial.print ("\n");
+
+}
