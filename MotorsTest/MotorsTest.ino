30c4cd7f4bba2d9fb661bf02eb20f4bb2f1d3aef
Luis Santos <lasantos2@outlook.com>
2016-12-13 16:04:26 -0700

    Cleaned up a bit, committing to start a new branch to try something

--- MotorsTest/MotorsTest.ino
+++ MotorsTest/MotorsTest.ino
@@ -1,21 +1,20 @@
 #include "Arduino.h"
 #include "motors.h"    //This library will contain the motors_set and button_set functions
 
-
 void setup() {
 
 	motors_set(11, 12, 9, 10, 15, 14);
 	button_set(2, 3, 4, 5, 6);
-  encoder_read(7, 8);
-  setupMPU();
-  Serial.begin(9600);
-  
+	encoder_read(7, 8);
+	setupMPU();
+	Serial.begin(9600);
+
 }
-  
+
 void loop() {
-	//The buttons are controlled via interrupts so there loop is empty.
- recordAccelRegisters();
- recordGyroRegisters();
- ir_read(7);
+	
+	recordAccelRegisters();
+	recordGyroRegisters();
+	ir_read(7);
 
 }
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Repository that will keep all my notes and code for the making of a micromouse
 
 
 Added l293D pinout image into the motortest folder
+
+
+
