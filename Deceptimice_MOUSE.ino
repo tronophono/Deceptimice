744752eb175321e8389ce38a7ea075bd5f37e9f3
Luis Santos <lasantos2@outlook.com>
2017-01-11 14:01:26 -0700

    Uploaded the main code that I have so far again

--- Deceptimice_MOUSE.ino
+++ Deceptimice_MOUSE.ino
@@ -1,33 +1,44 @@
-//#include "/home/lasantos/Desktop/Micromouse/Furios7-Micromouse/Furios7-Libraries/Dispatch/Dispatch.h"
-#include "/home/lasantos/Desktop/Micromouse/Furios7-Micromouse/Furios7-Libraries/GyroAccel/GyroAccel.h"
-int front_in = 15;
-int left_in = 17;
-int right_in = 16;
+#include "Arduino.h"
+#include "Dispatch.h"
+byte left = A0;
+byte right = A2;
+byte front = A1;
 
-unsigned long interval = 1000;
-unsigned long previoustime = 0;
-/*
- int MOTOR1PIN1 = 11;
- int MOTOR1PIN2 = 12;
- int MOTOR2PIN1 = 9;
- int MOTOR2PIN2 = 10;
- int ENABLEPIN1 = 8;
- int ENABLEPIN2 = 7;
- */
+Dispatch IR(front, left, right);
 
-//Dispatch IR(front_in, left_in, right_in);
-GyroAccel gyro;
+//0-forward, 1-turnleft,2-turnright, 3-all sided blocked
+byte directionState = 0;
 
 void setup() {
-	Serial.begin(9600);
+
+  Serial.begin(9600);
+  while (millis() != 1000)
+    ;
+  while (IR.Averagestate == false) {
+    Serial.print("IR Calibrated");
+    IR.AverageTolly();
+  }
+
 }
 
 void loop() {
+  Serial.print(IR.IR_middleavg);
+  delay(1000);
+}
 
-
-	gyro.recordGyroRegisters();
-	gyro.printgyro();
-	delay(1000);
-
+void checkingStates() {
+  if (IR.state_left and IR.state_mid) {
+    directionState = 2;
+    Serial.println("	Turning Right		");
+  } else if (IR.state_left and IR.state_right) {
+    directionState = 0;
+    Serial.println("			Going Forward		");
+  } else if (IR.state_mid and IR.state_right) {
+    directionState = 1;
+    Serial.println("         Turning left    ");
+  } else if (IR.state_mid and IR.state_left and IR.state_right) {
+    directionState = 3;
+    Serial.println("           Stopping   ");
+  }
 }
 
